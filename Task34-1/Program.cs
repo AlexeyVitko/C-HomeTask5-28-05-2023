81b90072214dd59cc81a4ed459907542853395f1
AlexVi <vpa24082012@gmal.com>
2023-05-30 23:21:32 +0300

    Добавили решение задачи 34-1

---
+++ Task34-1/Program.cs
@@ -0,0 +1,50 @@
+﻿// / Задача 34: 
+// 1. Задайте массив заполненный случайными положительными трёхзначными числами. 
+// 2. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+
+int[] array = CreateArrayRndInt(size, 100, 1000);
+Console.WriteLine($"Массив состоит из {size} элементов, из них {EvenNumber(array)} четные числа");
+Console.Write("[");
+PrintArray(array, ",");
+Console.WriteLine($"] -> {EvenNumber(array)}");
+
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max);
+    }
+    return arr;
+}
+
+
+void PrintArray(int[] arr, string sep) //sep - параметр для разделения (запятая и прочие)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+
+int EvenNumber(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) count++;
+    }
+    return count;
+}
